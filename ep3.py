73ab0bb0465eb2b929ea6e05fe2268b7f93a2230
赵子豪 <zihao13969918264@hotmail.com>
2017-04-26 16:42:35 +0800

    Create ep3.py

---
+++ ep3.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8-*-
+print "I will now count my chickens:"			#打印出“现在我要数我的小鸡数量”
+
+print "Hens", 25.0 + 30.0 / 6.0			#打印出 “母鸡”后面输出数量
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0			#打印出“公鸡”后面输出数量
+
+print "Now I will count the eggs:"			#打印出“现在我要数鸡蛋的个数”
+
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0			#打印出鸡蛋的个数
+
+print "Is it true that 3.0 + 2.0 < 5.0 - 7.0 ?"			#打印出“3+2<5-7是不是对的 ”
+
+print 3.0 + 2.0 < 5.0 - 7.0						#这是什么？为什么不能输出。
+
+print "What is 3.0 + 2.0?", 3.0 + 2.0			#打印“3+2是什么”，后面输出结果
+print "What is 5.0 - 7.0?", 5.0 - 7.0			#打印“5-7是什么”，后面输出结果
+
+print "Oh, that's why it's False."			
+
+print "How about some more."
+
+print "Is is greater?", 5.0 >= -2.0			#对问题进行判断，并判断后面的结果在问题中的真假值
+print "Is it greater or equal?", 5.0 >= -2.0
+print "Is it less or equal?", 5.0 <= -2.0
