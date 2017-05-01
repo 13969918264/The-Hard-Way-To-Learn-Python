fc8625168ab142f08ccb67b7288a90f719bcf600
赵子豪 <zihao13969918264@hotmail.com>
2017-05-01 08:41:00 +0800

    Create ep7.py

---
+++ ep7.py
@@ -0,0 +1,25 @@
+# -*- coding : uft-8 -*-
+#打印下列字符串常量
+print "Mary had a little lamb."
+print "Its fleece was white as %s ." % 'snow'			#将字符串变量直接赋值到格式控制符位置
+print "And everywhere that Mary went."
+# 打印“.”字符并连续输出10次。
+print "." * 10 # what'd that do?
+
+# 给下列字符变量赋值
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end. try removing it to see what happens
+# 将上面的字符用连接符连接起来形成一个字符串。
+print end1 + end2 + end3 + end4 + end5 + end6, print end7 + end8 + end9 + end10 + end11 + end12
