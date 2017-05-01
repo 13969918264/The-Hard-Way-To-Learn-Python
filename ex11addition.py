59756eb28a1ba4e7211d06e7dba5b7e7d3ccddc0
赵子豪 <zihao13969918264@hotmail.com>
2017-05-01 11:01:56 +0800

    Create ex11addition.py

---
+++ ex11addition.py
@@ -0,0 +1,10 @@
+print "Do you love a girl?",
+answer = raw_input()
+print "What's her name?",
+herName = input()
+print "Can you answer the question?"
+print "1.5 + 2 + 3",
+answer2 = input()
+answer3 = raw_input()
+
+print "you love a girl. %s.\nHer name is %s.\n %r.\n%r." % (answer, herName, answer2, answer3)
