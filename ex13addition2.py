4a04d2bf01342f4d0e57a2060412c4bc68e68bea
赵子豪 <zihao13969918264@hotmail.com>
2017-05-02 21:47:22 +0800

    Create ex13addition2.py

---
+++ ex13addition2.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+
+from sys import argv
+
+script, name, age, weight, height, girlfriendsname = argv
+
+print "The script is called:", script
+print "What's your name?", name
+print "How old are you?", age
+print "How much do you weith?", weight
+print "How tall are you?", height
+print "What's your girlfriends'name?", girlfriendsname
